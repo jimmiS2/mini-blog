41b289c50c9bc519bec7073965fe6f1d5b81d236
jimmiS2 <gyfl23242@naver.com>
2025-02-17 12:22:00 +0900

    init

---
+++ src/components/list/ComentList.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+import styled from 'styled-components';
+import CommentListItem from './CommentListItem';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    aligh-items: flex-start;
+    justify-content: center;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+function CommentList(props) {
+    const { comments } = props;
+
+    return (
+        <Wrapper>
+            {comments.map((coment) => {
+                return <CommentListItem key={coment.id} comment={comment} />;
+            })}
+        </Wrapper>
+    );
+}
+
+export default CommentList;
